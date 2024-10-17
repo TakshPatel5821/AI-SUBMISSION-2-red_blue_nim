d19cb6c122e695cf00ebfd7f3c70a6f2a7c29eb7
PATEL TAKSH KUMAR GIRISHBHAI <taksh5821@gmail.com>
2024-10-16 20:34:49 -0500

    this is a main code

---
+++ Readme.txt
@@ -0,0 +1,18 @@
+Name -  Patel TakshKumar Girishbhai
+UTA ID - 1002248623
+Language Used - Python 3.10.9
+
+Code Structure : 
+1. state_evaluation(red, blue, is_misere): Calculate the score of the current state based on the quantity of marbles in the red and blue piles.
+2. next_move(red, blue, pile, count): Update the number of marbles in the piles after a move is made.
+3. generate_move(red, blue): Generate all feasible moves in the given state.
+4. comp_move(red, blue, is_misere, depth): Use Minimax to determine the optimal move for the computer player.
+5. minimax(red, blue, is_misere, depth, is_maximizing): Recursive function to determine the optimal move using Minimax.
+6. main(): The main function to run the game.
+
+How to run the code : 
+1. Run the file red_blue_nim.py present in the zip folder.
+2. Input the required data in the format given in the question. 
+For eg. Run this script in the terminal - python red_blue_nim.py 5 6 standard human 2 (where red marbles are 5, blue marbles are 6, version is standard, first player will be human and depth taken is 2)
+3. The game will start with the first player as human and you'll have to play the game till any one of the marbles(blue or red) count gets zero.
+4. The end result will be generated, stating who won the game(human or computer) and their corresponding score.
---
+++ red_blue_nim.py
@@ -0,0 +1,132 @@
+import sys
+import math
+
+class RedBlueNim:
+    def __init__(self, red_marbles, blue_marbles):
+        self.red_marbles = red_marbles
+        self.blue_marbles = blue_marbles
+        self.current_player = 0  # 0 for human, 1 for computer
+
+    def is_game_over(self):
+        return self.red_marbles == 0 or self.blue_marbles == 0
+
+    def get_winner_score(self):
+        if self.red_marbles == 0:
+            return 3 * self.blue_marbles
+        elif self.blue_marbles == 0:
+            return 2 * self.red_marbles
+        else:
+            return None
+
+    def generate_moves(self, version):
+        if version == "standard":
+            moves = [(2, 0), (0, 2), (1, 0), (0, 1)]
+        else:
+            moves = [(0, 1), (1, 0), (0, 2), (2, 0)]
+        valid_moves = []
+        for move in moves:
+            if (move[0] > 0 and move[0] <= self.red_marbles) or (move[1] > 0 and move[1] <= self.blue_marbles):
+                valid_moves.append(move)
+        return valid_moves
+
+    def make_move(self, move):
+        red_remove, blue_remove = move
+        self.red_marbles -= red_remove
+        self.blue_marbles -= blue_remove
+        self.current_player = 1 - self.current_player  # Switch player
+
+    def evaluate(self):
+        if self.current_player == 0:
+            return 2 * self.red_marbles + 3 * self.blue_marbles
+        else:
+            return 2 * self.red_marbles + 3 * self.blue_marbles
+
+def minimax(game, depth, alpha, beta, maximizing_player):
+    if game.is_game_over() or depth == 0:
+        return game.evaluate(), None
+
+    if maximizing_player:
+        max_eval = -math.inf
+        best_move = None
+        for move in game.generate_moves("standard"):
+            game.make_move(move)
+            eval, _ = minimax(game, depth - 1, alpha, beta, False)
+            game.undo_move(move)
+            if eval > max_eval:
+                max_eval = eval
+                best_move = move
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return max_eval, best_move
+    else:
+        min_eval = math.inf
+        best_move = None
+        for move in game.generate_moves("misere"):
+            game.make_move(move)
+            eval, _ = minimax(game, depth - 1, alpha, beta, True)
+            game.undo_move(move)
+            if eval < min_eval:
+                min_eval = eval
+                best_move = move
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return min_eval, best_move
+
+def play_red_blue_nim(red_marbles, blue_marbles, version="standard", first_player="computer", depth=None):
+    game = RedBlueNim(red_marbles, blue_marbles)
+    if first_player == "computer":
+        current_player = 1
+    else:
+        current_player = 0
+
+    while not game.is_game_over():
+        if game.current_player == current_player:
+            if current_player == 0:
+                print(f"\nCurrent state: Red: {game.red_marbles}, Blue: {game.blue_marbles}")
+                valid_move = False
+                while not valid_move:
+                    print("Your turn:")
+                    red_remove = int(input("Remove how many red marbles (1 or 2)? "))
+                    blue_remove = int(input("Remove how many blue marbles (1 or 2)? "))
+                    if (red_remove, blue_remove) in game.generate_moves(version):
+                        valid_move = True
+                    else:
+                        print("Invalid move! Try again.")
+                game.make_move((red_remove, blue_remove))
+            else:
+                _, best_move = minimax(game, depth, -math.inf, math.inf, True)
+                game.make_move(best_move)
+                print(f"Computer removed {best_move[0]} red marbles and {best_move[1]} blue marbles.")
+        else:
+            _, best_move = minimax(game, depth, -math.inf, math.inf, True)
+            game.make_move(best_move)
+
+    winner_score = game.get_winner_score()
+    if winner_score is None:
+        print("It's a draw!")
+    elif winner_score > 0:
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: red_blue_nim.py <num-red> <num-blue> [<version>] [<first-player>] [<depth>]")
+        sys.exit(1)
+
+    red_marbles = int(sys.argv[1])
+    blue_marbles = int(sys.argv[2])
+    version = "standard"
+    first_player = "computer"
+    depth = None
+
+    if len(sys.argv) >= 4:
+        version = sys.argv[3]
+    if len(sys.argv) >= 5:
+        first_player = sys.argv[4]
+    if len(sys.argv) >= 6:
+        depth = int(sys.argv[5])
+
+    play_red_blue_nim(red_marbles, blue_marbles, version, first_player, depth)
